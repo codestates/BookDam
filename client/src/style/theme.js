3079e65654c110552218a3b64715b0863abf195c
HYONEE <hellohk07@gmail.com>
2022-01-01 16:13:40 +0900

    Add src/style/theme.js, Use ThemeProvider to index.js, Set dummyfiles

---
+++ client/src/style/theme.js
@@ -0,0 +1,12 @@
+// 반응형 모바일 사이즈 설정
+// index.js에 전체 포맷을 ThemeProvider로 설정
+const size = {
+  mobile: '770px'
+};
+
+const theme = {
+  backgroundColor: '#5dc175',
+  mobile: `(max-width: ${size.mobile})`
+};
+
+export default theme;

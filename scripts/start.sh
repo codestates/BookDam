5db913da93a96c1fcde606e8c1ae3a127e16501f
sangkwonkim <sangkwon2406@naver.com>
2022-01-01 15:21:42 +0900

    [Server] Add database tables, Fix scripts/start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/BookDam/server
 authbind --deep pm2 start app.js
-sudo pm2 ls
---
+++ server/migrations/20220101054448-create-follow.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Follows', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_Id: {
+        type: Sequelize.INTEGER
+      },
+      follow_Id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Follows');
+  }
+};

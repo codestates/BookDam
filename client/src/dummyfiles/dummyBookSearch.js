40f19a8f4173f6ff00e791fe079b202aab2a716a
Seunghoya <viva.primavera121@gmail.com>
2022-01-01 14:58:17 +0900

    [Client] Add dummydata

---
+++ client/src/dummyfiles/dummyBookSearch.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    title: "총, 균, 쇠 (무기 병균 금속은 인류의 운명을 어떻게 바꿨는가,퓰리처상 수상작)",
+    image: "http://book.naver.com/bookdb/book_detail.naver?bid=2084345",
+    author: "재레드 다이아몬드(대학교수) 저 | 김진준 역",
+    publisher: "문학사상사"
+  },
+  {
+    title: "총, 균, 쇠 (발간 20주년 기념 Gift Edition,GUNS, GERMS, AND STEEL, 기프트 에디션)",
+    image: "http://book.naver.com/bookdb/book_detail.naver?bid=12768582",
+    author: "재레드 다이아몬드(대학교수) 저 | 김진준 역",
+    publisher: "문학사상"
+  },
+  {
+    title: "Guns, Germs and Steel (A short history of everybody for the last 13,000 years,총, 균, 쇠)",
+    image: "http://book.naver.com/bookdb/book_detail.naver?bid=7052192",
+    author: "재레드 다이아몬드(대학교수) 저",
+    puclisher: "VintageBooks"
+  }
+]

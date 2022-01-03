ffed6647275fec8df43314dd88c9c20b0de6bf84
HYONEE <hellohk07@gmail.com>
2022-01-03 17:33:43 +0900

    [Cleint] Add Header,Footer components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import FeedPage from '../src/pages/FeedPage';
 import MyPage from '../src/pages/MyPage';
+import Footer from '../src/components/Footer/Footer';
 
 function App () {
   return (
     <>
-      We are Second Wind
+      <Header />
+        
       <Switch>
         <Route exact path='/' />
         <Route path='/feedpage'>
@@ -17,6 +19,8 @@ function App () {
           <MyPage />
         </Route>
       </Switch>
+
+      <Footer />
     </>
   );
 }

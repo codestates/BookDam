07345133ece7e6a9d3904639c5dbf5cb9e7b9ba4
HYONEE <hellohk07@gmail.com>
2022-01-05 17:37:59 +0900

    [Client][WIP] Add UserInfoModify Modal

---
+++ client/src/components/UserInfoModify/UserModifyModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  UserInfoModifyModalContainer,
+  UserInfoModifyContainer,
+} from './UserModifyModalStyle';
+
+
+export default function UserModifyModal () {
+
+  return (
+    <>
+      <UserInfoModifyModalContainer>
+        <UserInfoModifyContainer>
+          
+        </UserInfoModifyContainer>
+      </UserInfoModifyModalContainer>
+    </>
+  )
+}
+  

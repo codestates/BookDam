f61c8530e894c45cf0a65c23302403c7f88d5a16
HYONEE <hellohk07@gmail.com>
2022-01-06 16:46:49 +0900

    [Client] Fix UserInfoModifyModal component & [WIP] UserModifyModal

--- client/src/components/UserInfoModify/UserModifyModal.js
+++ client/src/components/UserInfoModify/UserModifyModal.js
@@ -1,14 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import {
   UserInfoModifyModalContainer,
-  UserInfoModifyContainer
+  UserInfoModifyContainer,
+  ModifyCloseSection,
+  UserInfoSection,
+  UserImgSection,
+  UserImage,
+  UserNickName,
+  UserInfoEditSection,
+  NickNameInput,
+  PasswordInput,
+  PasswordChkInput,
+  UserInfoModifyBtnSection,
+  ModificationBtn,
+  SignOutBtn
 } from './UserModifyModalStyle';
+import { Errormessage } from '../GlobalMessage/GlobalMessage';
+import { IoClose } from "react-icons/io5";
 
-export default function UserModifyModal () {
+// 회원정보수정 PATCH
+// http://localhost:4000/user/:user_Id
+// { userInfo: {userId:sangkwon2406, } }
+
+export function UserModifyModal ({ closeUserInfoModify }) {
+  const userState = useSelector(state => state.userInfoReducer);
+  const { userInfo } = userState; // 저장된 유저 정보
+  // input 값 유효성 검사 : 닉네임이 기존과 동일한가? 동일하면 에러메세지, password가 서로 일치한가? 불일치면 에러메세지
+  const [modifyUserInputInfo, setModifyUserInputInfo] = useState({
+    userId: '',
+    userNickName: '',
+    password: '',
+    userImage: ''
+  });
+  const { userNickName, password } = modifyUserInputInfo; // input 값으로 들어오는 정보
+  const [passwordChk, setPasswordChk] = useState(false);
+  const [nickNameErrorMessage, setNickNameErrorMessage] = useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [pwChkErrorMessage, setPwChkErrorMessage] = useState('');
+  const isValidPassword = /(?=.*\d)(?=.*[a-zA-ZS]).{8,}/; // 문자, 숫자 1개이상 포함, 8자리 이상
+
+  // 모달 창 닫는 함수
+  const closeModal = () => {
+    closeUserInfoModify();
+  };
+  // input handler
+  const handleInputNickName = (e) => {
+    if (userInfo.userNickName === e.target.value) {
+      setNickNameErrorMessage('기존과 동일한 닉네임입니다')
+    } else {
+      setNickNameErrorMessage('');
+      setModifyUserInputInfo({
+        userNickName: e.target.value,
+        password
+      });
+    }
+  };
+
+  const handleInputPW = (e) => {
+    if (e.target.value.length > 0 && isValidPassword.test(e.target.value) === false) {
+      setPasswordErrorMessage('8자리 이상의 문자+숫자 조합으로 만들어주세요')
+    } else {
+      setPasswordErrorMessage('');
+      setModifyUserInputInfo({
+        userNickName,
+        password: e.target.value
+      });
+    }
+  };
+
+  const handlePWCheck = (e) => {
+    if (e.target.value === password) {
+      setPwChkErrorMessage('');
+      setPasswordChk(true);
+    } else {
+      setPwChkErrorMessage('비밀번호가 일치하지 않습니다')
+    }
+  };
+
+
+  const changeUserInfo = () => {
+
+  }
+  
+  
   return (
     <>
       <UserInfoModifyModalContainer>
-        <UserInfoModifyContainer />
+        <UserInfoModifyContainer>
+          <ModifyCloseSection onClick={closeModal}>
+            <IoClose />
+          </ModifyCloseSection>
+          <UserInfoSection>
+            <UserImgSection>
+              <UserImage />
+            </UserImgSection>
+            <UserNickName>{userInfo.userNickName}</UserNickName>
+          </UserInfoSection>
+          <UserInfoEditSection>
+            <NickNameInput onChange={handleInputNickName} />
+            <Errormessage>{nickNameErrorMessage}</Errormessage>
+            <PasswordInput onChange={handleInputPW} />
+            <Errormessage>{passwordErrorMessage}</Errormessage>
+            <PasswordChkInput onChange={handlePWCheck} />
+            <Errormessage>{pwChkErrorMessage}</Errormessage>
+            <UserInfoModifyBtnSection>
+              <ModificationBtn>회원정보 수정</ModificationBtn>
+              <SignOutBtn>회원탈퇴</SignOutBtn>
+            </UserInfoModifyBtnSection>
+          </UserInfoEditSection>
+        </UserInfoModifyContainer>
       </UserInfoModifyModalContainer>
     </>
   );
--- client/src/components/UserInfoModify/UserModifyModalStyle.js
+++ client/src/components/UserInfoModify/UserModifyModalStyle.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 // 모달: 전체 배경, background
 export const UserInfoModifyModalContainer = styled.div`
   position: fixed;
@@ -15,7 +16,7 @@ export const UserInfoModifyModalContainer = styled.div`
   }
 `;
 
-// 모달: UserModify 모달창 컨테이너
+// 모달: UserModify 모달창
 export const UserInfoModifyContainer = styled.div`
   position: fixed;
   left: 50%;
@@ -32,4 +33,149 @@ export const UserInfoModifyContainer = styled.div`
   @media screen and (max-width: 500px) {
     display: none;
   }
+`;
+
+export const ModifyCloseSection = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 3px;
+  font-size: 25px;
+  color: rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+`;
+
+export const UserInfoSection = styled.div`
+  display: flex;
+  height: 90px;
+  margin: 10px;
+  border: 1px solid blue;
+
+  /* @media (max-width: 500px) {
+    width: 480px;
+    padding: 0;
+  } */
+`;
+
+export const UserImgSection = styled.div`
+  width: 90px;
+  height: 100%;
+  margin-left: 15px;
+  margin-right: 15px;
+  border: 1px solid black;
+`;
+
+export const UserImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const UserNickName = styled.div`
+  display: flex;
+  font-size: 1.1rem;
+  justify-content: start;
+  align-items: center;
+  text-align: center;
+  margin-left: 5px;
+  border: 1px solid green;
+`;
+
+export const UserInfoEditSection = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  position: relative;
+  padding: 0 20px 32px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid skyblue;
+`;
+
+export const NickNameInput = styled.input.attrs({
+  placeholder: 'NickName',
+  type: 'text'
+})`
+  margin-top: 10px;
+  border-radius: 2px;
+  width: 60%;
+  height: 40px;
+  border: 1px solid #e5e5e5;
+  padding: 9px 12px;
+  outline: none;
+  box-sizing: border-box;
+
+`;
+
+export const PasswordInput = styled.input.attrs({
+  placeholder: 'Password',
+  type: 'password'
+})`
+  margin-top: 10px;
+  border-radius: 2px;
+  width: 60%;
+  height: 40px;
+  border: 1px solid #e5e5e5;
+  padding: 9px 12px;
+  outline: none;
+  box-sizing: border-box;
+`;
+
+export const PasswordChkInput = styled.input.attrs({
+  placeholder: 'Password Check',
+  type: 'password'
+})`
+  margin-top: 10px;
+  border-radius: 2px;
+  width: 60%;
+  height: 40px;
+  border: 1px solid #e5e5e5;
+  padding: 9px 12px;
+  outline: none;
+  box-sizing: border-box;
+`;
+
+export const ErrorMessage = styled.div`
+
+`;
+
+export const UserInfoModifyBtnSection = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 90px;
+  margin-top: 20px;
+  /* border: 1px solid blue; */
+`;
+
+export const ModificationBtn = styled.button`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  border: 1px solid #12e272;
+  border-radius: 5px;
+  background-color: rgba(0,0,0,0);
+  color: #12e272;
+  font-weight: 500;
+  margin: 10px;
+  padding: 9px 12px;
+  border-radius: 2px;
+`;
+
+export const SignOutBtn = styled.button`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  border: 1px solid grey;
+  border-radius: 5px;
+  background-color: rgba(0,0,0,0);
+  color: rgba(0, 0, 0, 0.4);
+  margin: 10px;
+  padding: 9px 12px;
+  border-radius: 2px;
 `;

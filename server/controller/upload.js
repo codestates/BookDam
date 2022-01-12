80fcffcdb5476d086a5239407429a38f7fcc56de
sangkwonkim <sangkwon2406@naver.com>
2022-01-12 20:41:32 +0900

    [Server] Delete multer function

--- server/controller/upload.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  post: (req, res, next) => {
-    const Img = req.file;
-    console.log(Img);
-    // console.log(req)
-    // console.log('s3 이미지 경로 :',Img.location);
-  }
-};

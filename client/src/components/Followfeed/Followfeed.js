2637f8efbe08d41444e53ad7525a314dbc8f2944
Seunghoya <viva.primavera121@gmail.com>
2022-01-12 15:01:30 +0900

    [Client] Fix infinity Scroll Async

--- client/src/components/Followfeed/Followfeed.js
+++ client/src/components/Followfeed/Followfeed.js
@@ -1,15 +1,14 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect, useCallback, Suspense } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useInView } from "react-intersection-observer"
 import Axios from 'axios';
 import data from '../../dummyfiles/dummyFeedList';
 import { FaUserCheck } from 'react-icons/fa';
 import userImage from '../../assets/images/defaultUserImage.png';
-
 import { NoticeModal } from '../NoticeModal/NoticeModal'
 import { useDispatch } from 'react-redux';
 import { FollowInfoAction } from "../../actions/FollowInfoAction"
-
+import { Loading } from "../../utils/Loading/Loading"
 import {
   FeedContentContainer,
   UserInfoContainer,
@@ -46,6 +45,7 @@ export const Followfeed = ({ followFeedList }) => {
 
   // ref를 div에 걸어주면 해당 요소가 보일 때 inView가 true로, 안보이면 false로 바뀐다.
   const [ref, inView] = useInView()
+
   const [userInfo, setUserInfo] = useState({
     id: 1,
     userId: 'test1',
@@ -53,44 +53,25 @@ export const Followfeed = ({ followFeedList }) => {
     userImage: 'blahblahblahblah',
   })
 
-
-  // 스크롤 포인터로 확인하는 작업을 안거쳐도 됨
-  // const scrollHeight = document.documentElement.scrollHeight;
-  // const scrollTop = document.documentElement.scrollTop;
-  // const clientHeight = document.documentElement.clientHeight;
-  // console.log("scrollHeight", scrollHeight)
-  // console.log("scrollTop", scrollTop)
-  // console.log("clientHeight", clientHeight)
-
-  // useEffect(() => {
-  //   console.log("scrollHeight", scrollHeight)
-  //   console.log("scrollTop", scrollTop)
-  //   console.log("clientHeight", clientHeight)
-  //   console.log("리랜더링중")
-  // }, [scrollHeight, scrollTop, clientHeight])
-
-//   const handleScroll = () => {
-//   if(scrollTop + clientHeight >= scrollHeight) {
-  //   Axios.get(`http://localhost:4000/user/${userInfo.id}`, 
-  //     {
-  //       headers: { 'Contnet-Type': 'application/json' }
-  //     })
-  //     .then((data) => {
-  //       setFolowFeedLists([...followlist, ...data])
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-
-
-
   // 서버에서 아이템을 가지고 오는 함수
-  const getFolowFeedLists = useCallback(async () => {
+  const getFolowFeedLists = useCallback(() => {
     setLoading(true)
-    await Axios.get(`http://localhost:4000/article/${userInfo.id}?page=${page}`)
+    setTimeout(() => { 
+      Axios.get(`http://localhost:4000/article/${userInfo.id}?page=${page}` ,
+    {
+      headers: 
+      { 
+        'Contnet-Type': 'application/json',
+        withCredentials: true 
+      }
+    })
     .then((res) => {
       setFolowFeedLists(followFeedLists => [...followFeedLists, ...res.data.articleData])
     })
+    .catch((err)=> {
+      console.log(err)
+    })
+    }, 1000)
     setLoading(false)
   }, [page])
 
@@ -99,14 +80,12 @@ export const Followfeed = ({ followFeedList }) => {
     getFolowFeedLists()
   }, [getFolowFeedLists])
 
-
   useEffect(() => {
     // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
     if (inView && !loading) {
       setPage(prevState => prevState + 1)
     }
   }, [inView, loading])
-  console.log(page)
 
   const getFollowInfo = (el) => {
     console.log(el)
@@ -124,65 +103,39 @@ export const Followfeed = ({ followFeedList }) => {
     setInOpen(!isOpen)
   }
 
-  // 팔로우 피드 리스트 서버요청 함수
-  // const getFollowFeedList = () => {
-  //   Axios.get(`http://localhost:4000/user/${userInfo.id}`,
-  //     {
-  //       headers: { 'Contnet-Type': 'application/json' }
-  //     })
-  //     .then((data) => {
-  //       setFoolowFeedLists({
-  //         'Articles.user_Id': data['Articles.user_Id'],
-  //         'Articles.book_Title': data['Articles.book_Title'],
-  //         'Articles.book_Author': data['Articles.book_Author'],
-  //         'Articles.book_Thumbnail': data['Articles.book_Thumbnail'],
-  //         'Articles.book_Publisher': data['Articles.book_Publisher'],
-  //         'Articles.sentense': data['Articles.sentense'],
-  //         'Articles.comment': data['Articles.comment'],
-  //         'Articles.createdAt': data['Articles.createdAt'],
-  //         'Follows.user_Id': data['Follows.user_Id'], // 피드 작성자가 팔로우 한 아이디
-  //         'Follows.follow_Id': data['Follows.follow_Id'] // 피드 작성자 아이디 = id
-  //       })
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-
   // 팔로우 추가 함수
   const followHandler = (el) => {
     // el의 key값에 접근해서 핸들러 함수를 실행하면 개별실행도 가능해짐.
     // 실행할 때 조건문으로 article의 id가
-    // if (el['Articles.user_Id'] !== el['Follows.follow_Id']) {
-    //   Axios.post(`http://localhost:4000/follow/${el['Follows.user_Id']}?${el.id}`,
-    //   {
-    //     headers: { 'Contnet-Type': 'application/json' }
-    //   })
-    //   .then((data) => {
-    //     console.log(data)
-    //   })
-    //   .catch((err) => {
-    //     console.log(err)
-    //   })
-  // }
+    if (el["User.Follows.user_Id"] !== userInfo.id) {
+      Axios.post(`http://localhost:4000/follow/${el['Follows.user_Id']}?${el.id}`,
+      {
+        headers: { 'Contnet-Type': 'application/json' }
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 };
-  console.log(followFeedLists)
+  // console.log(followFeedLists)
   const feedList = followFeedLists.map((el, index) => {
     return (
       <UserInfoContainer key={index}>
         <UserInfo>
           <UserNameAndImage>
-
             <UserImageContainer onClick={() => getFollowInfo(el)} >
               {el["User.userImage"].length === 0
                 ? <UserImage src={el["User.userImage"]} />
-                : <DefaultUserImage src={userImage} />} {/* 경로문제는 추후 수정 필요함 */}
+                : <DefaultUserImage src={userImage} />}
             </UserImageContainer>
             <UserNickName onClick={() => getFollowInfo(el)} >
               {el["User.userNickName"]}
             </UserNickName>
             <UserFollowIcon value={el.id} onClick={() => followHandler(el)}>
               {el["User.Follows.user_Id"] === userInfo.id ? <FaUserCheck onClick={NoticeModalOpenHandler}/> : '팔로우'}
-
             </UserFollowIcon>
           </UserNameAndImage>
           <PostCreatedAt>
@@ -203,12 +156,13 @@ export const Followfeed = ({ followFeedList }) => {
   return (
     <>
       <FeedContentContainer>
-        {isOpen ? <NoticeModal NoticeModalOpenHandler={NoticeModalOpenHandler} followInfo={followInfo} /> : null}
-        {followFeedList.length === 0
-          ? <div>검색한 결과가 없습니다.</div>
+        {isOpen ? <NoticeModal NoticeModalOpenHandler={NoticeModalOpenHandler} userInfo={userInfo} followInfo={followInfo} /> : null}
+        {followFeedLists.length === 0
+          ? <div><Loading/></div>
           : <>
             {feedList}
           </>}
+          {loading ? <Loading /> : null}
       </FeedContentContainer>
     </>
   );

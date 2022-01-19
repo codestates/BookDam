cf0c088f5192599b1a0a7c1fc7fc62a4bd26f1ac
sangkwonkim <sangkwon2406@naver.com>
2022-01-19 20:08:17 +0900

    [Server] Fix scripts/start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,9 @@ export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 export ttbkey=$(aws ssm get-parameters --region ap-northeast-2 --names ttbkey --query Parameters[0].Value | sed 's/"//g')
+
+sleep 10s && pm2 status
 echo $DATABASE_HOST
+node models/index.js
 
 authbind --deep pm2 start app.js
